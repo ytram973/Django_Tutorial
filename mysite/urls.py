2a53d0e39703e4595d8df7e0a0177c973faa940f
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2026-02-26 11:54:54 +0100

    tuto 8 end toolbar

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
-]
+] + debug_toolbar_urls()
+
